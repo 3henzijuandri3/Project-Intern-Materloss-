c44e240d6a04a7883e1bb54165cd480554a89ff0
3henzijuandri3 <henzijuandri.work@gmail.com>
2021-08-24 23:36:56 +0700

    Deleting Scene Transition Node

--- UI/Script/Scene Transition.gd
+++ UI/Script/Scene Transition.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 
-func _ready() -> void:
+func transisi() -> void:
 	$AnimationPlayer.play("Fade_to_black")
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-	$AnimationPlayer.play("Fade_to_normal")
+	if anim_name == "Fade_to_black":
+		$AnimationPlayer.play("Fade_to_normal")
