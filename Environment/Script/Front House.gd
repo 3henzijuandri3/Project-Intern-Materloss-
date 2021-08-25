d45acdc8436b36195fc5434ec4797545b75041af
3henzijuandri3 <henzijuandri.work@gmail.com>
2021-08-25 15:14:55 +0700

    Testing the low light level

--- Environment/Script/Front House.gd
+++ Environment/Script/Front House.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 func _on_Pintu_Masuk_body_entered(body: Node) -> void:
 	if body.is_in_group("Player"):
-		$CanvasModulate/AnimationPlayer.play("Fade_to_black")
+		$CanvasModulate/AnimationPlayer.play("Fade_to_normal")
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-	if anim_name == "Fade_to_black":
-		$CanvasModulate/AnimationPlayer.play("Fade_to_normal")
+	if anim_name == "Fade_to_normal":
+		$CanvasModulate/AnimationPlayer.play("Fade_to_black")
 		get_tree().change_scene("res://Environment/Scene/Panda House.tscn")
